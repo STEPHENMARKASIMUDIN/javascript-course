55d8ba1950344c5af6001eefff9275d2d51ca7b5
Stephen Mark Asimudin <stephenmarkasimudin@gmail.com>
2020-02-06 11:27:43 +0800

    added submit function

--- js/main.js
+++ js/main.js
@@ -43,3 +43,42 @@ function Person(firstName, lastName, dob) {
 }
 //instantiate object
 const person1 = new Person('Stephen', 'Mark', '09/20/1993');
+
+const ul = document.querySelector('.my-list');
+ul.firstElementChild.textContent = 'Stephen Asi';
+ul.lastElementChild.innerHTML = 'Stephen M A';
+ul.children[1].innerHTML = 'Stephen M';
+ul.children[2].innerHTML = '<strong>Asi Steph</strong>';
+
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector('.my-list').style.background = '#333';
+//     document.querySelector('.my-form').classList.add('bg-dark');
+
+// });
+const myForm = document.querySelector('.my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const users = document.querySelector('.users');
+const msg = document.querySelector('.msg');
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    if (!nameInput.value || !emailInput.value) {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please fill all fields!';
+        setTimeout(() => msg.remove(), 2000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        users.appendChild(li);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+    // myList.firstElementChild.textContent = nameInput.value;
+    // myList.children[1].innerHTML = emailInput.value;
+    // setTimeout(() => myList.remove(), 2000);
+}
